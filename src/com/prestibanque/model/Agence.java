11c0d4a5873cd4a1989a504bb476affbd7b2ad67
HugoDumont44 <hugo.dumont@free.fr>
2015-08-05 17:08:56 +0200

    Create Agence

---
+++ src/com/prestibanque/model/Agence.java
@@ -0,0 +1,50 @@
+package com.prestibanque.model;
+
+public class Agence extends Banque {
+
+	/*ATTRIBUTS*/
+	private int identification;
+	private String nomAgence;
+	private String dateCreation;
+	
+	
+	/*CONSTRUCTEUR*/
+	public Agence(int identification, String nomAgence, String dateCreation) {
+		this.identification = identification;
+		this.nomAgence = nomAgence;
+		this.dateCreation = dateCreation;
+	}
+	public Agence(int identification, String nomAgence) {
+		this.identification = identification;
+		this.nomAgence = nomAgence;
+		this.dateCreation = "l'an de grâce 500 av JC";
+	}
+	
+	
+	/*METHODES D'ACCES AUX ATTRIBUTS*/
+	public int getIdentification() {
+		return identification;
+	}
+	public void setIdentification(int identification) {
+		this.identification = identification;
+	}
+	public String getNomAgence() {
+		return nomAgence;
+	}
+	public void setNomAgence(String nomAgence) {
+		this.nomAgence = nomAgence;
+	}
+	public String getDateCreation() {
+		return dateCreation;
+	}
+	public void setDateCreation(String dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+	
+	
+	/*COMPORTEMENTS DE LA CLASSE*/
+	public String demandeAudit (){
+		return "Super banque";
+	}
+	
+}
--- src/com/prestibanque/model/Banque.java
+++ src/com/prestibanque/model/Banque.java
@@ -2,4 +2,30 @@ package com.prestibanque.model;
 
 public class Banque {
 
+	/*ATTRIBUTS*/
+	private int nbAgence;
+	private String nomAgence;
+	
+	
+	/*CONSTRUCTEUR*/
+	public Banque() {
+		this.nbAgence = 5;
+		this.nomAgence = "PrestiBanque";
+	}
+	
+	
+	/*METHODES D'ACCES AUX ATTRIBUTS*/
+	public int getNbAgence() {
+		return nbAgence;
+	}
+	public void setNbAgence(int nbAgence) {
+		this.nbAgence = nbAgence;
+	}
+	public String getNomAgence() {
+		return nomAgence;
+	}
+	public void setNomAgence(String nomAgence) {
+		this.nomAgence = nomAgence;
+	}
+	
 }
